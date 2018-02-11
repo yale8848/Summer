2583b0e805b24dd3e80ea023928850d9db2f269a
yale8848 <royal8848@163.com>
2018-02-11 19:26:26 +0800

    1.1.6

--- src/main/java/ren/yale/java/event/EventMessage.java
+++ src/main/java/ren/yale/java/event/EventMessage.java
@@ -1,5 +1,7 @@
 package ren.yale.java.event;
 
+import java.util.ArrayList;
+
 /**
  * Yale
  *
@@ -10,6 +12,94 @@ public class EventMessage {
     private String key="";
     private String key1="";
     private String key2="";
+
+    private ArrayList<String>  strings;
+    private ArrayList<Integer>  ints;
+    private ArrayList<Long> longs;
+    private ArrayList<Float> floats;
+    private ArrayList<Double> doubles;
+
+
+    public EventMessage addStr(String v){
+        if (strings==null){
+            strings = new ArrayList<String>();
+        }
+        strings.add(v);
+        return this;
+    }
+
+    public EventMessage addInt(Integer v){
+        if (ints==null){
+            ints = new ArrayList<Integer>();
+        }
+        ints.add(v);
+
+        return this;
+    }
+
+    public EventMessage addLong(Long v){
+        if (longs==null){
+            longs = new ArrayList<Long>();
+        }
+        longs.add(v);
+        return this;
+    }
+
+    public EventMessage addFloat(Float v){
+        if (floats==null){
+            floats = new ArrayList<Float>();
+        }
+        floats.add(v);
+        return this;
+    }
+
+    public EventMessage addDouble(Double v){
+        if (doubles==null){
+            doubles = new ArrayList<Double>();
+        }
+        doubles.add(v);
+        return this;
+    }
+
+    public Integer getInteger(){
+        return getInteger(0);
+    }
+
+    public Integer getInteger(int pos){
+        if (ints==null||ints.size()<=pos){
+            return null;
+        }
+        return ints.get(pos);
+    }
+    public Long getLong(){
+
+        return getLong(0);
+    }
+    public Long getLong(int pos){
+        if (longs==null||longs.size()<=pos){
+            return null;
+        }
+        return longs.get(pos);
+    }
+    public Float getFloat(){
+
+        return getFloat(0);
+    }
+    public Float getFloat(int pos){
+        if (floats==null||floats.size()<=pos){
+            return null;
+        }
+        return floats.get(pos);
+    }
+    public Double getDouble(){
+        return getDouble(0);
+    }
+    public Double getDouble(int pos){
+        if (doubles==null||doubles.size()<=pos){
+            return null;
+        }
+        return doubles.get(pos);
+    }
 
     public String getKey1() {
         return key1;
@@ -42,7 +132,7 @@ public class EventMessage {
         return success;
     }
 
-    public EventMessage setSuccess(boolean success) {
+    public EventMessage success(boolean success) {
         this.success = success;
         return this;
     }
@@ -62,14 +152,19 @@ public class EventMessage {
         return object;
     }
 
-    public EventMessage setObject(Object object) {
+    public EventMessage object(Object object) {
         this.object = object;
         return this;
     }
 
     public static EventMessage message(Object o){
         EventMessage eventMessage = new EventMessage();
-        eventMessage.setObject(o);
+        if (o!=null){
+            eventMessage.object(o);
+        }
         return eventMessage;
+    }
+    public static EventMessage message(){
+        return message(null);
     }
 }
